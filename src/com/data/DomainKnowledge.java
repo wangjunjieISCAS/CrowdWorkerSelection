73aa0a3e91572e39bac2b67b430b537f28f88c16
WangJunjie <junjie@nfs.iscas.ac.cn>
2017-12-06 14:03:15 -0500

    update data src

---
+++ src/com/data/DomainKnowledge.java
@@ -0,0 +1,19 @@
+package com.data;
+
+import java.util.ArrayList;
+
+public class DomainKnowledge {
+	ArrayList<String> domainKnowledge;	
+	
+	public DomainKnowledge ( ArrayList<String> domainKnowledge ) {
+		this.domainKnowledge = domainKnowledge;
+	}
+
+	public ArrayList<String> getDomainKnowledge() {
+		return domainKnowledge;
+	}
+
+	public void setDomainKnowledge(ArrayList<String> domainKnowledge) {
+		this.domainKnowledge = domainKnowledge;
+	}
+}
