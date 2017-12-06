13696daac3c10167e085a2a9af9bf9f19977014b
Jianfeng <ginfungchan@gmail.com>
2017-12-06 11:24:50 -0500

    add nsgaii algorithm

--- src/com/selectionApproach/JmetalProblem.java
+++ src/com/selectionApproach/JmetalProblem.java
@@ -1,5 +1,6 @@
 package com.selectionApproach;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -16,7 +17,7 @@ import jmetal.util.JMException;
 public class JmetalProblem extends Problem {
 	private static final long serialVersionUID = -3530441329740021364L;
 	private ArrayList<String> ids;
-	
+
 	public JmetalProblem(ArrayList<String> candidatesIDs) {
 		this.numberOfObjectives_ = 3;
 		this.numberOfVariables_ = candidatesIDs.size();
@@ -36,16 +37,16 @@ public class JmetalProblem extends Problem {
 	@Override
 	public void evaluate(Solution solution) throws JMException {
 		SortedMap<String, Boolean> selectionChoice = this.candidateMap(solution);
-		
-		double bugProb = SelectionObjectives.extractBugProbability(selectionChoice );
-		double diversity = SelectionObjectives.extractDiversity( selectionChoice);
-		double cost = SelectionObjectives.extractCost( selectionChoice);
-		
-		solution.setObjective(0, bugProb );
+		double bugProb = SelectionObjectives.extractBugProbability(selectionChoice);
+		double diversity = SelectionObjectives.extractDiversity(selectionChoice);
+		double cost = SelectionObjectives.extractCost(selectionChoice);
+
+		solution.setObjective(0, bugProb);
 		solution.setObjective(1, diversity);
 		solution.setObjective(2, cost);
-		
-		System.out.println ( "bugProb is: " + bugProb + ". Diversity is: " + diversity + ". Cost is: " + cost );
+
+		// System.out.println("bugProb is: " + bugProb + ". Diversity is: " +
+		// diversity + ". Cost is: " + cost);
 	}
 
 	public int getLength(int var) {
@@ -53,13 +54,22 @@ public class JmetalProblem extends Problem {
 	}
 
 	public static void main(String[] args) throws ClassNotFoundException, JMException {
-		ArrayList<String> testing = new ArrayList<String>();
-		testing.add("a");
-		testing.add("b");
-		testing.add("c");
-		testing.add("d");
-		
-		JmetalProblem google = new JmetalProblem(testing);
+		File f = new File("data/input/candidates.csv");
+		String line;
+		BufferedReader br;
+		ArrayList<String> candidates = new ArrayList<String>();
+
+		try {
+			br = new BufferedReader(new FileReader(f));
+			while ((line = br.readLine()) != null) {
+				candidates.add(line.trim());
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		JmetalProblem google = new JmetalProblem(candidates);
 		Solution randS = new Solution(google);
 		google.evaluate(randS);
 	}
--- src/com/selectionApproach/MultiObjectiveSelection.java
+++ src/com/selectionApproach/MultiObjectiveSelection.java
@@ -2,51 +2,80 @@ package com.selectionApproach;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.SortedMap;
+import java.util.HashMap;
 
-import com.data.Constants;
-import com.data.CrowdWorker;
-import com.data.Phone;
+import jmetal.core.Algorithm;
+import jmetal.core.SolutionSet;
+import jmetal.operators.crossover.SinglePointCrossover;
+import jmetal.operators.mutation.BitFlipMutation;
+import jmetal.operators.selection.Selection;
+import jmetal.operators.selection.SelectionFactory;
+import jmetal.qualityIndicator.QualityIndicator;
+import jmetal.util.JMException;
+import jmetal.util.PseudoRandom;
 
 public class MultiObjectiveSelection {
 
-	public void multiObjectiveWorkerSelection ( ArrayList<String> candidatesIDs ) {
-		
+	public SolutionSet multiObjectiveWorkerSelection(ArrayList<String> candidatesIDs, long seed)
+			throws ClassNotFoundException, JMException {
+		JmetalProblem problem_ = new JmetalProblem(candidatesIDs);
+		PseudoRandom.setRandomGenerator(new MyRandomGenerator(seed));
+		Algorithm alg = new NsgaiiWithDebug(problem_);
+
+		// TODO change the optimizer here
+		/** for all MOEA parameters **/
+		// TODO set up all parameter here
+		alg.setInputParameter("populationSize", 50);
+		alg.setInputParameter("maxEvaluations", 50 * 10);
+
+		/**
+		 * apply naive crossover and mutation; apply binary domination as
+		 * default in jMetal
+		 **/
+		HashMap<String, Object> parameters = new HashMap<String, Object>();
+
+		parameters.clear();
+		parameters.put("probability", 0.8);
+		alg.addOperator("crossover", new SinglePointCrossover(parameters));
+
+		parameters.clear();
+		parameters.put("probability", 0.2);
+		alg.addOperator("mutation", new BitFlipMutation(parameters));
+
+		parameters.clear();
+		Selection selection = SelectionFactory.getSelectionOperator("BinaryTournament2", parameters);
+		alg.addOperator("selection", selection);
+
+		SolutionSet res = alg.execute();
+
+		return res;
 	}
-	
-	public ArrayList<String> obtainCandidateIDs ( ) {
+
+	public static ArrayList<String> obtainCandidateIDs() {
 		ArrayList<String> candidatesIDs = new ArrayList<String>();
-		
+
 		BufferedReader reader;
 		try {
-			reader = new BufferedReader(new FileReader( new File ( "data/input/candidates.csv" )));
-			
+			reader = new BufferedReader(new FileReader(new File("data/input/candidates.csv")));
+
 			String line = "";
-			while ( ( line = reader.readLine() ) != null ) {
+			while ((line = reader.readLine()) != null) {
 				String userId = line.trim();
-				candidatesIDs.add( userId );				
-			}		
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				candidatesIDs.add(userId);
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return candidatesIDs;
 	}
-	
-	public static void main ( String[] args ) {
+
+	public static void main(String[] args) throws ClassNotFoundException, JMException {
 		MultiObjectiveSelection selectionTool = new MultiObjectiveSelection();
-		ArrayList<String> candidateIDs = selectionTool.obtainCandidateIDs();
-		System.out.println( candidateIDs.size() );
-		
-		
+		ArrayList<String> candidateIDs = MultiObjectiveSelection.obtainCandidateIDs();
+		selectionTool.multiObjectiveWorkerSelection(candidateIDs, 12345L);
 	}
 }
